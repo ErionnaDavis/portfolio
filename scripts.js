a8db4ef5165411d9b4e967f97a104aa8330d18c0
Erionna Davis <erionnadavis15@gmail.com>
2017-07-17 13:23:10 -0500

    js update

--- scripts.js
+++ scripts.js
@@ -1,9 +1,15 @@
+$(document).ready(function () {
+    $("#contactForm").on('submit', contactSubmit);
+});
 function contactSubmit() {
+    event.preventDefault();
     var x = document.forms['contactForm']['email'].value;
     if (x == "") {
         alert("email must be filled out");
         return false;
     } else {
-       alert('Thank You For Contacting Erionna Davis!!!'); 
+        $(this).fadeOut();
+        $("#success").fadeIn();
+        //alert('Thank You For Contacting Erionna Davis!!!');
     }
 }
