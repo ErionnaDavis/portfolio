73a9170dd479dd1c2549d00f2ac891c809a70d28
Erionna Davis <erionnadavis15@gmail.com>
2017-07-19 13:37:10 -0500

    animated

--- scripts.js
+++ scripts.js
@@ -1,4 +1,25 @@
+var projects=[
+    {
+        "name":"H2o Pub And Grill &#9774;"
+    },{
+        "name":"Try.Jquery &#9992;"
+    },{
+        "name": "Hospitality&Tourism &#9992;"
+
+    },{ "name": "Business Managment &#9743;"
+
+    },{ "name":"Computer Networking &#9773"
+
+    },{"name":"Electricity/Electronics &#10002"
+
+    }
+];
 $(document).ready(function () {
+    $.each(projects, function(index, value){
+        console.log(value);
+        $("#projects").append('<li>' + value.name + '</li>');
+
+    });
     $("#contactForm").on('submit', contactSubmit);
 });
 function contactSubmit() {
@@ -9,8 +30,9 @@ function contactSubmit() {
         return false;
     } else {
         $(this).fadeOut();
-        $("#success").fadeIn();
+        $ ("#success").fadeIn();
         //alert('Thank You For Contacting Erionna Davis!!!');
+        
           
 
     }
